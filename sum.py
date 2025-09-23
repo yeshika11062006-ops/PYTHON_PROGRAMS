143604b3a6a7e21e6cb40cc31e65141b6a24e77f
yeshika11062006-ops <yeshika11062006@gmail.com>
2025-09-23 23:36:35 +0530

    Add hello.py and sum.py

---
+++ sum.py
@@ -0,0 +1,10 @@
+# sum.py
+# Take input from the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Calculate sum
+total = num1 + num2
+
+# Print result
+print("Sum:", total)
