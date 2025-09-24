6696d2721114aa5ab2cdbbad5ef7080566a53116
yeshika11062006-ops <yeshika11062006@gmail.com>
2025-09-24 22:54:31 +0530

    Add files via upload
    add ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+# Program to demonstrate if-else statements
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("The number is positive.")
+elif num == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
